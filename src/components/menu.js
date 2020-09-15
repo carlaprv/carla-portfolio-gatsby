a5dd66f3c26025720ea74cb1fc4a6664daa21b3d
Carla Vieira <carlaprv@hotmail.com>
2020-09-15 13:24:20 -0300

    feat: add full banner video and responsive menu (#1)
    * add full banner on index page
    
    * responsive full banner
    
    * video on full banner
    
    * video on full banner
    
    * remove fullheader not used css
    
    * remove fullheader not used css
    
    * remove fullheader not used css
    
    * fix warnings
    
    * change bio pt
    
    * fix grid layout for menu
    
    * css changes on card, header and language
    
    * fix full banner responsive height
    
    * doc string on scss files
    
    * create menu component
    
    * add hamburger button
    
    * add hamburger button

---
+++ src/components/menu.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import Language from "./language"
+import menuStyles from './menu.module.scss'
+import classNames from 'classnames';
+
+const Menu = () => {
+      const [menuActive, setMenuActive] = useState(false);
+      const data = useStaticQuery(graphql`
+        query {
+            site{
+                siteMetadata{
+                    title
+                    menuLinks {
+                        name
+                        link
+                      }
+                }
+            }
+        }
+    `)
+
+      return (
+            <nav className={menuStyles.navContainer}>
+                  
+                  {/* Brand and toggle get grouped for better mobile display  */}
+                  <Link className={menuStyles.navBrand} to="/">{data.site.siteMetadata.title}</Link>
+                  <Language />
+
+                  {/* Button for mobile */}
+                  <button onClick={() => setMenuActive(!menuActive)} className={classNames({[menuStyles.activeOne]: menuActive})}>
+                        <span className={classNames({[menuStyles.hamburger]: true, [menuStyles.hamburgerOne]: true})}></span>
+                  </button>
+
+                  {/* Collect the nav links and content for toggling */}
+                  <ul className={classNames({[menuStyles.navList]: true, [menuStyles.toogle]: menuActive})}>
+                        {data.site.siteMetadata.menuLinks.slice(0).reverse().map((menuItem) => {
+                              return(
+                                    <li>
+                                          <Link className={menuStyles.navItem} activeClassName={menuStyles.activeNavItem} to={menuItem.link}>{menuItem.name}</Link>
+                                    </li>
+                              )
+                        })}
+                  </ul>
+            </nav>
+      )
+}
+
+export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -30,12 +30,42 @@ const IndexPage = () => {
     `)
 	return(
 		<Layout>
+			<section>
+				<video poster="" id="bgvid" loop muted autoPlay>
+					<source src="/images/video-carla-banner.mp4" type="video/mp4"/>
+				</video>
+				<div className={indexStyles.viewportHeader}>
+					<div className={indexStyles.fullHeaderContainer}>
+						<h1>
+							{intl.formatMessage({ id: "greeting" })}
+						</h1>
+						<h2>
+							{intl.formatMessage({ id: "headline" })}
+						</h2>
+						<ul className={indexStyles.socialIcons}>
+							{data.site.siteMetadata.socialLinks.map((socialItem) => {
+								return(
+								<li>
+									<a href={socialItem.link} target="_blank" rel="noreferrer">
+										<img src={socialItem.icon} alt={socialItem.name}/>
+									</a>
+								</li>
+								)
+							})}
+						</ul>
+						<button className={indexStyles.headerButton} href="/">
+							<a href="/">{intl.formatMessage({ id: "cta" })}</a>
+						</button>
+					</div>
+				</div>
+
+			</section>
 			<section className={layoutStyles.coloredSection}>
-				<div className={indexStyles.headerContainer}>
-					<div className={indexStyles.headerImage}>
+				<div className={indexStyles.aboutContainer}>
+					<div className={indexStyles.aboutImage}>
 						<img src={data.site.siteMetadata.image}/>
 					</div>
-					<div className={indexStyles.headerText}>
+					<div className={indexStyles.aboutText}>
 						<h1>{data.site.siteMetadata.author}</h1>
 						<h2 className={indexStyles.headline}>
 							{intl.formatMessage({ id: "headline" })}
