bb80baa2ade0b2692e4bad03b49a341fbbcccc4a
Carla Vieira <carlaprv@hotmail.com>
2020-09-18 22:55:55 -0300

    update graphql query to list blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,15 +7,19 @@ import blogStyles from './blog.module.scss'
 const BlogPage = () => {
 	const data = useStaticQuery(graphql`
 		query {
-			allMarkdownRemark {
+			allMarkdownRemark(
+				sort: { fields: frontmatter___date, order: DESC }
+			) {
 				edges {
 					node {
-						frontmatter {
-							title
-							date
-						}
 						fields {
 							slug
+						}
+						frontmatter {
+							date(formatString: "DD MMM[,] YYYY")
+							description
+							title
+							language
 						}
 					}
 				}
