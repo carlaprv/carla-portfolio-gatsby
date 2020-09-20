7e70d03a199a718ffae04af09a9026f0ee500e74
Carla Vieira <carlaprv@hotmail.com>
2020-09-20 19:37:56 -0300

    add graphql query outside blog list component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,35 +4,14 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 import blogStyles from './blog.module.scss'
 
-const BlogPage = () => {
-	const data = useStaticQuery(graphql`
-		query {
-			allMarkdownRemark(
-				sort: { fields: frontmatter___date, order: DESC }
-			) {
-				edges {
-					node {
-						fields {
-							slug
-						}
-						frontmatter {
-							date(formatString: "DD MMM[,] YYYY")
-							description
-							title
-							language
-						}
-					}
-				}
-			}
-		}
-	  `)
+const BlogPage = (props) => {
 	
 	return (
 		<Layout>
 			<h1>Blog</h1>
 			<p>Posts here</p>
 			<ol className={blogStyles.posts}>
-				{data.allMarkdownRemark.edges.map((edge) => {
+				{props.data.allMarkdownRemark.edges.map((edge) => {
 					return(
 						<li className={blogStyles.post}>
 							<Link to={`/blog/${edge.node.fields.slug}`}>
@@ -49,4 +28,27 @@ const BlogPage = () => {
 	)
 }
 
+export const query = graphql`
+	query {
+		allMarkdownRemark(
+			sort: { fields: frontmatter___date, order: DESC }
+		) {
+			edges {
+				node {
+					fields {
+						slug
+					}
+					frontmatter {
+						date(formatString: "DD MMM[,] YYYY")
+						description
+						title
+						language
+					}
+				}
+			}
+		}
+	}
+`
+
 export default BlogPage
+
