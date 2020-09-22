d28bd6be1aa76779408b09a87677a19829334106
Carla Vieira <carlaprv@hotmail.com>
2020-09-22 15:38:22 -0300

    style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link } from 'gatsby'
+
 
 import layoutStyles from '../components/layout.module.scss'
-import indexStyles from './index.module.scss'
 import errorStyles from './404.module.scss'
 import { useIntl } from "gatsby-plugin-intl"
 
@@ -13,14 +14,24 @@ const NotFoundPage = () => {
       return (
             <Layout>
                   <section className={layoutStyles.coloredSection}>
-                        <div className={indexStyles.aboutContainer}>
+                        <div className={layoutStyles.sectionContent}>
                               <div className={errorStyles.content}>
                                     {intl.locale === "pt" ? (
+                                          <>
                                           <h1>Não tem nada aqui...</h1>
+                                          <img src="/images/avatar_carla_sad_transparent.png" alt="Ilustração da carla com uma carinha triste."/>
+                                          <h2>Mas não se preocupe!</h2>
+                                          <p>Que tal dar uma lida no meu <Link to="/blog">blog</Link>?</p>
+                                          </>
                                     ):(
-                                          <h1>Page Not Found</h1>
+                                          <>
+                                          <h1>404: Page Not Found</h1>
+                                          <img src="/images/avatar_carla_sad_transparent.png" alt="Illustration of Carla with a sad face."/>
+                                          <h2>Don't worry!</h2>
+                                          <p>What about reading my <Link to="/blog">blog</Link>?</p>
+                                          </>
                                     )}
-                                    <img src="/images/avatar_carla_sad_transparent.png" alt=""/>
+                                    
                               </div>
                         </div>
                   </section>
