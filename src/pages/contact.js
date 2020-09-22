f7cbac8afba950aab6b90385c228e7ef1b9185ce
Carla Vieira <carlaprv@hotmail.com>
2020-09-21 21:59:23 -0300

    create contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,12 +7,31 @@ import { useIntl } from "gatsby-plugin-intl"
 const ContactPage = () => {
 	const intl = useIntl()
 
-	return(
+	return (
 		<Layout>
-			<SEO title={intl.formatMessage({ id: "contact" })}/>
+			<SEO title={intl.formatMessage({ id: "contact" })} />
 			<h1>Contact</h1>
 			<p>You can send an e-mail to: contato@carlavieira.dev</p>
 			<p><a href="https://twitter.com/carlaprvieira">Twitter</a></p>
+
+			<form name="contact" method="POST" data-netlify="true">
+				<p>
+					<label>Your Name: <input type="text" name="name" /></label>
+				</p>
+				<p>
+					<label>Your Email: <input type="email" name="email" /></label>
+				</p>
+				<p>
+					<label>Subject: <input type="text" name="subject" /></label>
+				</p>
+				<p>
+					<label>Message: <textarea name="message"></textarea></label>
+				</p>
+				<p>
+					<button type="submit">Send</button>
+				</p>
+			</form>
+
 		</Layout>
 	)
 }
