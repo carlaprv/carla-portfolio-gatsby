d2cc70a06e8500d8ae4e985e405df9553a11feab
Carla Vieira <carlaprv@hotmail.com>
2020-09-22 10:48:36 -0300

    style card and add image

--- src/components/card.js
+++ src/components/card.js
@@ -2,35 +2,25 @@ import React from 'react'
 // import { Link, graphql, useStaticQuery } from 'gatsby'
 import cardStyles from './card.module.scss'
 
-const Card = () => {
+const Card = ({
+      slug,
+      date,
+      title,
+      description
+    }) => {
 
       return(
             <div>
                   <ul className={cardStyles.cards}>
                         <li className={cardStyles.cards__item}>
                               <div className={cardStyles.card}>
-                                    <div className={cardStyles.card__content}>
-                                    <div className={cardStyles.card__title}><a href="/">Flex</a></div>
-                                    <p className={cardStyles.card__text}>This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
-                                    <button className={cardStyles.btn}>Button</button>
+                                    <div className={cardStyles.card__image}>
+                                          <img src="https://unsplash.it/800/600?image=59"/>
                                     </div>
-                              </div>
-                        </li>
-                        <li className={cardStyles.cards__item}>
-                              <div className={cardStyles.card}>
                                     <div className={cardStyles.card__content}>
-                                    <div className={cardStyles.card__title}><a href="/">Flex</a></div>
-                                    <p className={cardStyles.card__text}>This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
-                                    <button className={cardStyles.btn}>Button</button>
-                                    </div>
-                              </div>
-                        </li>
-                        <li className={cardStyles.cards__item}>
-                              <div className={cardStyles.card}>
-                                    <div className={cardStyles.card__content}>
-                                    <div className={cardStyles.card__title}><a href="/">Flex</a></div>
-                                    <p className={cardStyles.card__text}>This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
-                                    <button className={cardStyles.btn}>Button</button>
+                                          <div className={cardStyles.card__title}><a href="/">Flex</a></div>
+                                          <p className={cardStyles.card__text}>This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+                                          <button className={cardStyles.btn}>Button</button>
                                     </div>
                               </div>
                         </li>
