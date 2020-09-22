909ac86d7d56303143c453404a608a9715c0df3d
Carla Vieira <carlaprv@hotmail.com>
2020-09-21 22:43:36 -0300

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,39 +1,81 @@
 import React from "react"
+import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { useIntl } from "gatsby-plugin-intl"
+import contactStyles from './contact.module.scss'
+import layoutStyles from '../components/layout.module.scss'
 
-
-const ContactPage = () => {
+const ContactPage = (props) => {
 	const intl = useIntl()
 
 	return (
 		<Layout>
 			<SEO title={intl.formatMessage({ id: "contact" })} />
-			<h1>Contact</h1>
-			<p>You can send an e-mail to: contato@carlavieira.dev</p>
-			<p><a href="https://twitter.com/carlaprvieira">Twitter</a></p>
+			<section className={layoutStyles.coloredSection}>
+        			<div className={layoutStyles.sectionContent}>
+					<div className={contactStyles.text}>
+						<h1>Contact</h1>
+						<p>Looking to get in touch? I'd love to hear from you. The best way to get in touch with me is to send me an e-mail to <a href="mailto:contato@carlavieira.dev">contato@carlavieira.dev</a> or fill out the form below.</p>
+					</div>
 
-			<form name="contact" method="POST" data-netlify="true">
-				<p>
-					<label>Your Name: <input type="text" name="name" /></label>
-				</p>
-				<p>
-					<label>Your Email: <input type="email" name="email" /></label>
-				</p>
-				<p>
-					<label>Subject: <input type="text" name="subject" /></label>
-				</p>
-				<p>
-					<label>Message: <textarea name="message"></textarea></label>
-				</p>
-				<p>
-					<button type="submit">Send</button>
-				</p>
-			</form>
-
+					<div className={contactStyles.contact}>
+						<div className={contactStyles.socialInfo}>
+							<ul className={contactStyles.socialIcons}>
+								{props.data.site.siteMetadata.socialLinks.map((socialItem) => {
+									return (
+										<li>
+											<a href={socialItem.link} target="_blank" rel="noreferrer">
+												<img src={socialItem.svg} alt={socialItem.name} alt="" />
+											</a>
+										</li>
+									)
+								})}
+							</ul>
+						</div>
+						<div className={contactStyles.contactForm}>
+							<form name="contact" method="POST" data-netlify="true">
+								<p>
+									<label>{intl.formatMessage({ id: "name" })}: <input type="text" name="name" /></label>
+								</p>
+								<p>
+									<label>Email: <input type="email" name="email" /></label>
+								</p>
+								<p>
+									<label>{intl.formatMessage({ id: "subject" })}: <input type="text" name="subject" /></label>
+								</p>
+								<p>
+									<label>{intl.formatMessage({ id: "message" })}: <textarea name="message"></textarea></label>
+								</p>
+								<p>
+									<button type="submit">Send</button>
+								</p>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
 		</Layout>
 	)
 }
 
 export default ContactPage
+
+export const query = graphql`
+	query {
+		site{
+			siteMetadata{
+				author	
+				image
+				socialLinks {
+					name
+					link
+					icon
+					svg
+				}
+			}
+		}
+	}
+
+`
