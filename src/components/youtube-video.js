a94657d15b5ad30fedfc4454f151dbb6fa9017d4
Carla Vieira <carlaprv@hotmail.com>
2020-09-25 15:51:01 -0300

    fix youtube grid

--- src/components/youtube-video.js
+++ src/components/youtube-video.js
@@ -3,36 +3,27 @@ import youtubeVideoStyles from './youtube-video.module.scss'
 
 
 const YoutubeVideo = ({
-      youtube,
-    }) => {
+	youtube
+}) => {
 
-      return(
-            <>
-            <ul className={youtubeVideoStyles.cards}>
-                  {youtube.map((edge) => {
-                        return (
-                                    <li className={youtubeVideoStyles.cards__item}>
-                                          <div className={youtubeVideoStyles.card}>
-                                                <div className={youtubeVideoStyles.card__image}>
-                                                      <a href={edge.node.frontmatter.link}><img src={edge.node.frontmatter.thumbnail.publicURL} alt=""/></a>
-                                                </div>
-                                                {/* <div className={youtubeVideoStyles.card__content}>
-                                                      <div className={youtubeVideoStyles.card__title}>
-                                                            <a href={edge.node.frontmatter.link}>{edge.node.frontmatter.title}</a>
-                                                      </div>
-                                                      {/* <p className={youtubeVideoStyles.card__text}>{edge.node.frontmatter.description}</p> */}
-                                                      {/* <div className={youtubeVideoStyles.card__date}>
-                                                            {edge.node.frontmatter.date} */}
-                                                      {/* </div> */} 
-                                                {/* </div> */}
-                                                
-                                          </div>
-                                    </li>
-                              )
-                  })}
-            </ul>   
-            </>
-      )
+	return (
+		<>
+			<ul className={youtubeVideoStyles.cards}>
+				{youtube.map((edge) => {
+					return (
+						<li className={youtubeVideoStyles.cards__item}>
+							<div className={youtubeVideoStyles.card}>
+								<a href={edge.node.frontmatter.link}>
+									<span className={youtubeVideoStyles.card__label}> youtube </span>
+									<img src={edge.node.frontmatter.thumbnail.publicURL} alt="" />
+								</a>
+							</div>
+						</li>
+					)
+				})}
+			</ul>
+		</>
+	)
 }
 
 export default YoutubeVideo;
