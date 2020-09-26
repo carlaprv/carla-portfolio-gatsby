6f117196f4af33bd84b636ed08b6ec098276733f
Carla Vieira <carlaprv@hotmail.com>
2020-09-26 11:36:02 -0300

    create global style

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Header from './header'
 import Footer from './footer'
-import '../styles/index.scss'
+import GlobalStyle from '../styles/globalStyle';
 import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
     return(
         <div className={layoutStyles.container}>
+            <GlobalStyle/>
             <Header/>
             <div className={layoutStyles.content}>
                 {props.children}
