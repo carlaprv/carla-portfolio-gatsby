5f1412963ba98ec08a3dc649d2fd90c2ce5d1847
Carla Vieira <carlaprv@hotmail.com>
2020-10-16 09:37:40 -0300

    remove unused image

--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -9,24 +9,6 @@ import PageHeader from '../components/page-header'
 
 import layoutStyles from '../components/layout.module.scss'
 
-const SpeakingContent = styled.div`
-	display: flex;
-	align-items: center;
-`
-
-const SpeakingText = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-`
-
-const SpeakingImage = styled.img`
-	margin-left:30px;
-	width:300px;
-	flex-grow:0;
-      flex-shrink:0;
-	border-radius: 6px;
-`
 const SpeakingPage = () => {
 	const intl = useIntl()
 
@@ -41,23 +23,18 @@ const SpeakingPage = () => {
 			/>
 			<section className={layoutStyles.coloredSection}>
 				<div className={layoutStyles.sectionContent}>
-					{/* <SpeakingContent> */}
-						{/* <SpeakingText> */}
-							{intl.locale === "pt" ? (
-								<><p>
-									Já participei de diferentes eventos como palestrante tanto no Brasil quanto fora. Se você tem interesse nas palestras que eu já dei, os slides estão disponíveis no speakerdeck. 
-								</p><p>
-									Se você tem interesse em uma palestra minha na sua conferência/evento ou quer apenas entrar em contato, pode só enviar um e-mail para <a href="mailto:contato@carlavieira.dev">contato@carlavieira.dev</a> ou preencher o <Link to="/contact">formulário de contato</Link>.
-								</p></>
-							) : (
-								<p>
-									I've given quite a few talks around the world about Python, Artificial Intelligence, Data science and more.
-									If you're interested in me speaking at your conference or podcast, you can send an e-mail to <a href="mailto:contato@carlavieira.dev">contato@carlavieira.dev</a> or fill out my <Link to="/contact">contact form</Link>.
-								</p>
-							)}
-						{/* </SpeakingText>
-						<SpeakingImage src="images/carla-palestra-cp-goias.jpg"/> */}
-					{/* </SpeakingContent> */}
+						{intl.locale === "pt" ? (
+							<><p>
+								Já participei de diferentes eventos como palestrante tanto no Brasil quanto fora. Se você tem interesse nas palestras que eu já dei, os slides estão disponíveis no speakerdeck. 
+							</p><p>
+								Se você tem interesse em uma palestra minha na sua conferência/evento ou quer apenas entrar em contato, pode só enviar um e-mail para <a href="mailto:contato@carlavieira.dev">contato@carlavieira.dev</a> ou preencher o <Link to="/contact">formulário de contato</Link>.
+							</p></>
+						) : (
+							<p>
+								I've given quite a few talks around the world about Python, Artificial Intelligence, Data science and more.
+								If you're interested in me speaking at your conference or podcast, you can send an e-mail to <a href="mailto:contato@carlavieira.dev">contato@carlavieira.dev</a> or fill out my <Link to="/contact">contact form</Link>.
+							</p>
+						)}
 				</div>
 			</section>
 		</Layout>
