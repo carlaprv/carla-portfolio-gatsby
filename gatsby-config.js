256b044882ec086897cd84bc17b0dfe117df5a83
Carla Vieira <carlaprv@hotmail.com>
2021-02-04 17:41:24 -0300

    add google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -169,6 +169,31 @@ module.exports = {
 				redirect: false,
 			},
 		},
+		{
+		resolve: `gatsby-plugin-google-gtag`,
+		options: {
+			// You can add multiple tracking ids and a pageview event will be fired for all of them.
+			trackingIds: [
+				"UA-135620629-1", // Google Analytics / GA
+			],
+			// This object gets passed directly to the gtag config command
+			// This config will be shared across all trackingIds
+			// gtagConfig: {
+			// 	optimize_id: "OPT_CONTAINER_ID",
+			// 	anonymize_ip: true,
+			// 	cookie_expires: 0,
+			// },
+			// This object is used for configuration specific to this plugin
+			pluginConfig: {
+				// Puts tracking script in the head instead of the body
+				// head: false,
+				// // Setting this parameter is also optional
+				// respectDNT: true,
+				// Avoids sending pageview hits from custom paths
+				exclude: ["/preview/**", "/do-not-track/me/too/", "/404/"],
+			},
+		},
+		},
 		'gatsby-plugin-sharp',
 		{
 			resolve: 'gatsby-transformer-remark',
